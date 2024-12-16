d636887d4e41686874ed6fc1d000f00e7fb743af
mlengse <medtosys@gmail.com>
2024-12-16 14:38:55 +0700

    asdad

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,42 +2,32 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeObsidian from 'starlight-theme-obsidian';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      plugins: [starlightThemeObsidian()],
-      title: 'Referensi',
-      social: {
-        github: 'https://github.com/mlengse',
+  integrations: [starlight({
+    plugins: [starlightThemeObsidian()],
+    title: 'Referensi',
+    social: {
+      github: 'https://github.com/mlengse',
+    },
+    sidebar: [
+      {
+        label: 'Akreditasi',
+        autogenerate: { directory: 'docs/akred' }
       },
-      sidebar: [
-        {
-          label: 'Akreditasi',
-          items: [
-            {
-              label: 'Puskesmas',
-              autogenerate: { directory: 'docs/akred/pkm' }
-            },
-            {
-              label: 'Klinik',
-              // slug: 'docs/akred/klinik',
-              autogenerate: { directory: 'docs/akred/klinik' }
-            }
-          ],
-        },
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'docs/guides/example' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'docs/reference' },
-        },
-      ],
-    }),
-  ],
+      {
+        label: 'Guides',
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: 'Example Guide', slug: 'docs/guides/example' },
+        ],
+      },
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'docs/reference' },
+      },
+    ],
+  }), markdoc()],
 });
