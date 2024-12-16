835393faf628efc9a0bea1ce5b42ea8382d7b4b3
mlengse <medtosys@gmail.com>
2024-12-16 11:33:24 +0700

    dasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
             },
             {
               label: 'Klinik',
+              // slug: 'docs/akred/klinik',
               autogenerate: { directory: 'docs/akred/klinik' }
             }
           ],
